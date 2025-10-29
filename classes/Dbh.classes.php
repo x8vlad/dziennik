077352bbd1236503f84b2a16b8266829cceaa277
x8vlad <volikovv38@gmail.com>
2025-10-29 21:02:05 +0100

    Start implementing Singltone pattern

--- classes/Dbh.classes.php
+++ classes/Dbh.classes.php
@@ -1,12 +1,23 @@
 <?php 
 class Dbh {
-    protected function connect(){
+    private static $singltoneDB;
+    private $connection;
+    
+    private function __construct()
+    {
         try{
-            $conn = new PDO("mysql:host=localhost;dbname=dziennik", "root", "");
-            return $conn;
+            $this->connection = new PDO("mysql:host=localhost;dbname=dziennik", "root", "");
         }catch(PDOException $error){
             print "Oops, error: " . $error->getMessage() . "<br>";
             die();
         }
     }
+
+    // для создания одного экземпляра класса
+    public static function getInstance(){
+        if(self::$singltoneDB == null) {self::$singltoneDB = new Dbh();}
+        return self::$singltoneDB;
+    }
+
+    public function connect(){return $this->connection;}
 }
--- database.php
+++
@@ -1,4 +0,0 @@
-<?php
-    // $conn = mysqli_connect('localhost', 'root', '', 'dziennik');
-    // $conn = new PDO("mysql:host=localhost;dbname=dziennik", "root", "");
-?>
