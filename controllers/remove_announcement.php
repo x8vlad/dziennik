7a63c672013a7b5b0df889aa6c1a16af693bc596
x8vlad <volikovv38@gmail.com>
2025-11-03 20:10:06 +0100

    Use singltone in controller Remove all announcement

--- controllers/remove_announcement.php
+++ controllers/remove_announcement.php
@@ -1,14 +1,14 @@
 <?php 
     include_once(__DIR__ . '/../config/config.php');
 
-    include_once(ROOT_PATH  . '/database.php');
+    include_once('../classes/Dbh.classes.php');
 ?>
 
 <?php
     sleep(1);
     $remove_data_query = "TRUNCATE TABLE `announcement`";
-    $stmt = $conn->prepare($remove_data_query);
-    $conn->exec($remove_data_query);
+    $stmt = Dbh::getInstance()->connect()->prepare($remove_data_query);
+    Dbh::getInstance()->connect()->exec($remove_data_query);
     //mysqli_query($conn, $remove_data_query);
     echo "ok";
 ?>
