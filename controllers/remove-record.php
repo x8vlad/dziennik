32ae68d37699635f98ce40b369e4a0c6531a2d2b
x8vlad <volikovv38@gmail.com>
2025-11-03 20:20:42 +0100

    Use singltone in remove record controller

--- controllers/remove-record.php
+++ controllers/remove-record.php
@@ -1,13 +1,13 @@
 <?php 
     include_once(__DIR__ . '/../config/config.php');
-    include_once(ROOT_PATH  . '/database.php');
+    include_once('../classes/Dbh.classes.php');
 ?>
 
 <?php
     $id = $_POST['id'] ?? null;
     sleep(1);
     $remove_this_record_query = "DELETE FROM `announcement` WHERE id=:id";
-    $stmt = $conn->prepare($remove_this_record_query);
+    $stmt = Dbh::getInstance()->connect()->prepare($remove_this_record_query);
     $stmt->bindValue(":id", $id);
     $stmt->execute();
 ?>
