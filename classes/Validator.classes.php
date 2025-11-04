6abc880b57a6cf5be2c760272a66e7366af782b7
x8vlad <volikovv38@gmail.com>
2025-11-04 11:35:57 +0100

    Implement a singltone to Validator

--- classes/Validator.classes.php
+++ classes/Validator.classes.php
@@ -1,5 +1,12 @@
 <?php
 class Validator{
+    private $instanceValidator;
+
+    public function getInstance(){
+        if(self::$instanceValidator == null) {self::$instanceValidator = new Validator();}
+        return self::$instanceValidator = new Validator();
+    }
+
     public function isEmptyData($data){
         if(empty($data)){
             // echo "Empty data";
