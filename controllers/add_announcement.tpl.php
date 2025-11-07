f80d45c6256a96b72270cd069aa60db3c198c27d
x8vlad <volikovv38@gmail.com>
2025-11-07 18:36:42 +0100

    Use a validator method in add announcement controller all works

--- controllers/add_announcement.tpl.php
+++ controllers/add_announcement.tpl.php
@@ -1,32 +1,37 @@
-<?php 
-    include_once(__DIR__ . '/../config/config.php');
-    include_once('../classes/Dbh.classes.php');
-    include_once('../classes/Validator.classes.php');
-    
+<?php
+include_once(__DIR__ . '/../config/config.php');
+include_once('../classes/Dbh.classes.php');
+include_once('../classes/Validator.classes.php');
+
 ?>
 
-<?php   
-    // $validator = new Validator();
-    if(isset($_POST['title']) && isset($_POST['content']) && Validator::getInstance()->isNotEmptyData($_POST['title']) && Validator::getInstance()->isNotEmptyData($_POST['content'])){    
+<?php
+// $validator = new Validator();
+if (isset($_POST['title']) && isset($_POST['content'])) {
+    if (Validator::getInstance()->isNotEmptyData($_POST['title']) && Validator::getInstance()->isNotEmptyData($_POST['content'])) {
         $query = "INSERT INTO `announcement` (title, content, created_at) VALUES (:title, :content, NOW())";
         //опредиляем prepared statment (чтобы уникать уязвиости кода)
         $stmt = Dbh::getInstance()->connect()->prepare($query);
         //  привязываем параметры (титл и контект) к значениям :title,:content
         $stmt->bindValue(":title", $_POST['title']);
         $stmt->bindValue(":content", $_POST['content']);
-        
+
         if ($stmt->execute()) {
-            header('Location: ' . BASE_URL .'view/ogloszenia.tpl.php');
-            exit(); 
+            header('Location: ' . BASE_URL . 'view/ogloszenia.tpl.php');
+            exit();
         }
+    }else{
+        echo "Some filed's empty";
     }
-    include_once(controller('header.php'));
+}
+
+include_once(controller('header.php'));
 ?>
 
-  <div class="container my-5">
+<div class="container my-5">
     <h1>Form for announcement</h1>
     <form method="POST" action="">
-         <div class="mb-3">
+        <div class="mb-3">
             <label class="form-label">Title</label>
             <input type="text" name="title" class="form-control">
         </div>
@@ -36,7 +41,7 @@
         </div>
         <button type="submit" class="btn btn-success" id="attempBtn" name="btn_submit">Attempt</button>
     </form>
-  </div>
-<?php 
-    include_once(view('footer.php'));
+</div>
+<?php
+include_once(view('footer.php'));
 ?>
