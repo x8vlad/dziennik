d23b9c20cb9d9b3ddf97cb6d6c190fa458af336a
x8vlad <volikovv38@gmail.com>
2025-12-04 18:28:24 +0100

    Check munuLinks, preparing for json request

--- controllers/get_attendance_link.php
+++ controllers/get_attendance_link.php
@@ -1,3 +1,7 @@
+<?php require_once(__DIR__ . '/../config/config.php'); ?>
+<?php require_once(__DIR__ . '/../controllers/header.php'); ?>
 <?php
-    
+echo "<pre>";
+    print_r($munuLinks);
+echo "</pre>";
 ?>
