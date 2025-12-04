f222db88ee7178bb15f091c5ffbac912c9fd7ac6
x8vlad <volikovv38@gmail.com>
2025-12-04 18:42:52 +0100

    get a lable and url, preparing to request json

--- controllers/get_attendance_link.php
+++ controllers/get_attendance_link.php
@@ -1,8 +1,20 @@
 <?php require_once(__DIR__ . '/../config/config.php'); ?>
 <?php require_once(__DIR__ . '/../controllers/header.php'); ?>
 <?php
-header("Content-Type: application/json");
+// header("Content-Type: application/json");
 // echo "<pre>";
 //     print_r($munuLinks);
 // echo "</pre>";
+$attendance_lable;
+$attendance_url;
+foreach($munuLinks as $munuLink){
+    if($munuLink['lable'] === "attendance" && $munuLink['url'] === '/ja/projectPHP/dziennik/view/attendance.tpl.php'){
+        $attendance_lable = $munuLink['lable'];
+        $attendance_url = $munuLink['url'];
+    }
+}
+echo $attendance_lable;
+echo "<br>";
+echo $attendance_url;
+
 ?>
