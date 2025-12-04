9e43ea59018e53352ba5a334e07f22b87447518b
x8vlad <volikovv38@gmail.com>
2025-12-04 18:30:53 +0100

    Implement a content type json

--- controllers/get_attendance_link.php
+++ controllers/get_attendance_link.php
@@ -1,7 +1,8 @@
 <?php require_once(__DIR__ . '/../config/config.php'); ?>
 <?php require_once(__DIR__ . '/../controllers/header.php'); ?>
 <?php
-echo "<pre>";
-    print_r($munuLinks);
-echo "</pre>";
+header("Content-Type: application/json");
+// echo "<pre>";
+//     print_r($munuLinks);
+// echo "</pre>";
 ?>
