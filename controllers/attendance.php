4b88129e31fbead5a8e92c30a8d56edd34d31f71
x8vlad <volikovv38@gmail.com>
2025-12-16 22:16:11 +0100

    select in table attendance all students

--- controllers/attendance.php
+++ controllers/attendance.php
@@ -1,5 +1,17 @@
 <?php
 include_once(__DIR__ . '/../config/config.php');
-include_once(__DIR__ . '/../config/config.php');
 include_once('../classes/Dbh.classes.php');
+
+$query = "SELECT login FROM `users` WHERE users.role = :student ORDER BY users.id ASC";
+$stmt = Dbh::getInstance()->connect()->prepare($query);
+$stmt->bindValue(":student", "student");
+$stmt->execute();
+
+$res = $stmt;
+
+while($row = $res->fetch(PDO::FETCH_ASSOC)){
+  echo '<tr>';
+  echo '<td>' . $row['login'] . '</td>';
+  echo '</tr>';
+}
 ?>
