d00bc819ced2965591060db7cb32f444145c470c
x8vlad <volikovv38@gmail.com>
2025-12-30 22:18:31 +0100

    Seperate logic from remove announcement controller to model

--- controllers/remove_announcement.php
+++ controllers/remove_announcement.php
@@ -1,12 +1,14 @@
 <?php 
-    include_once(__DIR__ . '/../config/config.php');
-    include_once('../classes/Dbh.classes.php');
+include_once(__DIR__ . '/../config/config.php');
+include_once('../classes/Dbh.classes.php');
+include_once('../models/removeAnnouncementModel.php');
+sleep(1);
+$removeAnnouncementObject = new removeAnnouncementModel();
+$removeAnnouncementObject->removeAllAnnouncement();
+    // $remove_data_query = "TRUNCATE TABLE `announcement`";
+    // $stmt = Dbh::getInstance()->connect()->prepare($remove_data_query);
+    // Dbh::getInstance()->connect()->exec($remove_data_query);
     
-    sleep(1);
-    $remove_data_query = "TRUNCATE TABLE `announcement`";
-    $stmt = Dbh::getInstance()->connect()->prepare($remove_data_query);
-    Dbh::getInstance()->connect()->exec($remove_data_query);
-    
-    echo "ok";
+    // echo "ok";
 ?>
 
--- models/removeAnnouncementModel.php
+++ models/removeAnnouncementModel.php
@@ -1,1 +1,10 @@
 <?php
+class removeAnnouncementModel{
+    public function removeAllAnnouncement(){
+        $remove_data_query = "TRUNCATE TABLE `announcement`";
+        if(Dbh::getInstance()->connect()->exec($remove_data_query)){
+            return true;
+        }else {return false;}
+        // return "ok";
+    }
+}
