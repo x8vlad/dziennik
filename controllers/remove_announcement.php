d75412f5b3048434e5d759d0034b090c22f9660f
x8vlad <volikovv38@gmail.com>
2025-12-30 22:07:41 +0100

    Create a new model for remove all  announcement Preparing to MVC

--- controllers/remove_announcement.php
+++ controllers/remove_announcement.php
@@ -1,15 +1,12 @@
 <?php 
     include_once(__DIR__ . '/../config/config.php');
-
     include_once('../classes/Dbh.classes.php');
-?>
-
-<?php
+    
     sleep(1);
     $remove_data_query = "TRUNCATE TABLE `announcement`";
     $stmt = Dbh::getInstance()->connect()->prepare($remove_data_query);
     Dbh::getInstance()->connect()->exec($remove_data_query);
-    //mysqli_query($conn, $remove_data_query);
+    
     echo "ok";
 ?>
 
