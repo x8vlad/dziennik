6e7d5ff8d8fabbf646d8fde371aeaf7dd19564d8
x8vlad <volikovv38@gmail.com>
2025-12-30 23:28:58 +0100

    Seperate logic from table users controller to model

--- controllers/table-usersAJAX.php
+++ controllers/table-usersAJAX.php
@@ -1,23 +1,28 @@
 <?php
 include_once(__DIR__ . '/../config/config.php');
 include_once('../classes/Dbh.classes.php');
+include_once('../models/tableUsersAJAXModel.php');
+    $showUserObject = new ShowUser();
     $select_option = "*";
     if(isset($_POST['userOption']) && !empty($_POST['userOption'])){
-    $select_option = $_POST['userOption'];
+        $select_option = $_POST['userOption'];
     } 
     //SELECT * FROM `users` WHERE role = ..
     if ($select_option == "*") {
-        $query = "SELECT id,login FROM `users` ORDER BY id ASC";
-        $stmt = Dbh::getInstance()->connect()->prepare($query);
-        $stmt->execute();
+        $result = $showUserObject->showAllUserTable();
+        // $query = "SELECT id,login FROM `users` ORDER BY id ASC";
+        // $stmt = Dbh::getInstance()->connect()->prepare($query);
+        // $stmt->execute();
     } else {
-        $query = "SELECT id,login FROM `users` WHERE role = :users ORDER BY id ASC";
-        $stmt = Dbh::getInstance()->connect()->prepare($query);
-        $stmt->bindValue(":users", $select_option);
-        $stmt->execute();
+        // $query = "SELECT id,login FROM `users` WHERE role = :users ORDER BY id ASC";
+        // $stmt = Dbh::getInstance()->connect()->prepare($query);
+        // $stmt->bindValue(":users", $select_option);
+        // $stmt->execute();
+        $result = $showUserObject->showСertainUserTable($select_option);
+   
     }
 
-    $result = $stmt;
+    // $result = $stmt;
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         echo '<tr>';
         echo '<td>' . $row['id'] . '</td>';
--- models/tableUsersAJAXModel.php
+++ models/tableUsersAJAXModel.php
@@ -1,10 +1,19 @@
 <?php
 class ShowUser{
     public function showAllUserTable(){
-        
+        $query = "SELECT id,login FROM `users` ORDER BY id ASC";
+        $stmt = Dbh::getInstance()->connect()->prepare($query);
+        $stmt->execute();
+        return $stmt;
     }
 
-    public function showСertainUserTable($role){
-
+    public function showСertainUserTable($roleSelected){
+        $query = "SELECT id,login FROM `users` WHERE role = :users ORDER BY id ASC";
+        $stmt = Dbh::getInstance()->connect()->prepare($query);
+        $stmt->bindValue(":users", $roleSelected);
+        $stmt->execute();
+        return $stmt;
     } 
+
+
 }
